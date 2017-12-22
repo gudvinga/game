57a429b8c3e04cd4a004f19eb18eabd5874c6e8e
gudvinga <gudvinga@gmail.com>
2017-12-22 05:52:26 +0300

    Lending update

--- source/js/index.js
+++ source/js/index.js
@@ -1,11 +1,11 @@
-import '../css/style.css';
+// import '../css/style.css';
 import 'phaser.min.js';
-import 'boot.js';
-import 'game.js';
-import 'load.js';
-import 'lose.js';
-import 'menu.js';
-import 'play.js';
-import 'win.js';
+// import 'boot.js';
+// import 'game.js';
+// import 'load.js';
+// import 'lose.js';
+// import 'menu.js';
+// import 'play.js';
+// import 'win.js';
 
 console.log('Hello webpack');
